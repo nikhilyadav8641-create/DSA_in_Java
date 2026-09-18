01e32196f67564ac5caf0b957bd26ec25120e564
Nikhil Yadav <nikhilyadav8641@gmail.com>
2026-09-18 19:51:43 +0530

    Time: 6 ms (47.51%), Space: 46.5 MB (97.52%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] arr, int tar) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] ans= new int[2];
+        for(int i=0;i<arr.length;i++)
+        {
+            int ele=tar-arr[i];
+            if(map.containsKey(ele) && map.get(ele)!=i)
+				{
+					ans[0]=i;
+                    ans[1]=(map.get(ele));
+					// return;
+				}
+            map.put(arr[i],i);
+            
+        }
+        return ans;
+    }
+}
